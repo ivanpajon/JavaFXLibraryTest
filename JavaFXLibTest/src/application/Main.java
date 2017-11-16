a91a4f874442c0c149bdcbf9b0bcfc80c968f2f3
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-16 01:26:19 +0100

    first commit

---
+++ JavaFXLibTest/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+	        Scene scene = new Scene(root);
+	        primaryStage.setScene(scene);
+	        primaryStage.initStyle(StageStyle.TRANSPARENT);  // Le quita los bordes a la ventana
+	        scene.setFill(Color.TRANSPARENT);  // Le quita los bordes a la ventana
+	        primaryStage.setTitle("Test");
+	        primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ JavaFXLibTest/src/application/MainController.java
@@ -0,0 +1,91 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import fxwindow.fxmove.FXMove;
+import fxwindow.fxresize.FXResize;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class MainController implements Initializable {
+
+    @FXML private Pane paneToolbar, paneUp, paneRight, paneDown, paneLeft, paneUpLeft, paneUpRight, paneDownRight, paneDownLeft;
+    
+    @FXML private AnchorPane root;
+    
+    private FXMove fxmove;
+    private FXResize fxresize;
+    
+    @Override
+	public void initialize(URL url, ResourceBundle rb) {
+    	fxmove = new FXMove(root);
+    	fxresize = new FXResize(root, paneUp, paneRight, paneDown, paneLeft, paneUpLeft, paneUpRight, paneDownRight, paneDownLeft);
+	}
+
+    @FXML void moveDragged(MouseEvent e) {
+    	fxmove.moveDragged(e);
+    }
+
+    @FXML void movePressed(MouseEvent e) {
+    	fxmove.movePressed(e);
+    }
+    
+ 	@FXML void moveReleased(MouseEvent e) {
+ 		if (fxmove.isDraggedToUp(e)) {
+ 			fxmove.dragToUp(e);
+ 			fxresize.disableResizing();
+ 		}
+ 		else {
+ 			fxmove.dragToCenter(e);
+ 		}
+ 	}
+    
+    @FXML void resizeVerticalPressed(MouseEvent e) {
+    	fxresize.resizeVerticalPressed(e);
+    }
+    
+    @FXML void resizeUpDragged(MouseEvent e) {
+    	fxresize.resizeUpDragged(e);
+    }
+    
+    @FXML void resizeDownDragged(MouseEvent e) {
+        fxresize.resizeDownDragged(e);
+    }
+    
+    @FXML void resizeHorizontalPressed(MouseEvent e) {
+        fxresize.resizeHorizontalPressed(e);
+    }
+    
+    @FXML void resizeLeftDragged(MouseEvent e) {
+        fxresize.resizeLeftDragged(e);
+    }
+    
+    @FXML void resizeRightDragged(MouseEvent e) {
+        fxresize.resizeRightDragged(e);
+    }
+    
+    @FXML void resizeCornerPressed(MouseEvent e) {
+    	fxresize.resizeCornerPressed(e);
+    }
+    
+    @FXML void resizeUpLeftDragged(MouseEvent e) {
+    	fxresize.resizeUpLeftDragged(e);
+    }
+    
+    @FXML void resizeUpRightDragged(MouseEvent e) {
+    	fxresize.resizeUpRightDragged(e);
+    }
+    
+    @FXML void resizeDownRightDragged(MouseEvent e) {
+    	fxresize.resizeDownRightDragged(e);
+    }
+    
+    @FXML void resizeDownLeftDragged(MouseEvent e) {
+    	fxresize.resizeDownLeftDragged(e);
+    }
+    
+}
