cd702bee592422b2f2dc33c365233833e4559a16
Ivan Zhen <ivangpajon@hotmail.com>
2017-11-21 19:39:01 +0100

    Updated SecondStageController

--- JavaFXLibTest/src/application/SecondStageController.java
+++ JavaFXLibTest/src/application/SecondStageController.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
 
 import fxwindow.fxmove.FXMove;
+import fxwindow.fxstage.FXStage;
 import fxwindow.fxtoolbar.FXToolbar;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -17,16 +18,19 @@ public class SecondStageController implements Initializable {
 	
 	@FXML private AnchorPane root;
 	
-	@FXML private JFXButton btnClose;
+	@FXML private JFXButton btnClose, btnUpdateScene;
 	
 	private FXMove fxmove;
 	private FXToolbar fxtoolbar;
+	private FXStage fxstage;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		fxmove = new FXMove(root);
 		
 		fxtoolbar = new FXToolbar(root);
+		
+		fxstage = new FXStage();
 	}
 	
 	@FXML void close(ActionEvent e) {
@@ -39,5 +43,10 @@ public class SecondStageController implements Initializable {
 
     @FXML void moveDragged(MouseEvent e) {
     	fxmove.moveDragged(e);
+    }
+    
+    @FXML void updateScene(ActionEvent e) {
+    	fxstage.setScene("src/application/Main.fxml");
+    	fxstage.updateScene(e);
     }
 }
